c93783c61436c621426cce8e35bdc33c5bddc389
naveen <135437477+naveen2728@users.noreply.github.com>
2024-11-12 21:03:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let a =document.getElementById("i");
+let b=document.getElementById("i2");
+let c =document.getElementById("b1");
+let  d=document.getElementById("p3");
+let  g=document.getElementById("p4");
+c.onclick=function(){
+    let e=a.value;
+    let f=b.value;
+    if(e!=="" && f!==""){
+        
+    
+    e=e*60*60;
+    f=f*60;
+    g.classList.add("p2");
+    g.textContent=e+f;
+    d.textContent="";
+    }else if(e==="" && f!==""){
+        d.textContent="Please Enter a Valid Number of minutes";
+    }
+    else if(f==="" && e!==""){
+        d.textContent="Please Enter a Valid Number of hours ";
+        
+    }
+    else{
+        d.textContent="Please Enter a Valid Number of hours and minutes";
+    }
+}
